9d3830354766d065e912c1b6f4d47d175997d336
Norio Nomura <norio.nomura@gmail.com>
2015-02-02 17:33:38 +0900

    Change `HMAC` from `class` to `struct`

--- HMAC/HMAC.swift
+++ HMAC/HMAC.swift
@@ -10,36 +10,40 @@ import Foundation
 
 // https://tools.ietf.org/html/rfc2202
 
-public class HMAC {
+public struct HMAC {
     public enum Algorithm: CUnsignedInt {
         case SHA1, MD5, SHA256, SHA384, SHA512, SHA224
     }
 
     typealias Context = UnsafeMutablePointer<HMAC_bridge_Context>
+    class CTX {
+        var context = Context.alloc(1)
+        deinit { context.dealloc(1) }
+    }
     
-    var context = Context.alloc(1)
+    let ctx = CTX()
     let algorithm: Algorithm
     
-    public convenience init(algorithm: Algorithm, key string: String) {
+    public init(algorithm: Algorithm, key string: String) {
         assert(!string.isEmpty, "key: String must not be empty.")
         let key = string.cStringUsingEncoding(NSUTF8StringEncoding)
         let length = string.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
         self.init(algorithm: algorithm, key: key!, keyLength: length)
     }
     
-    public convenience init(algorithm: Algorithm, key array: [UInt8]) {
+    public init(algorithm: Algorithm, key array: [UInt8]) {
         assert(array.count > 0, "key: Array.count must be greater than zero.")
         self.init(algorithm: algorithm, key: array, keyLength: array.count)
     }
     
-    public convenience init(algorithm: Algorithm, key data: NSData) {
+    public init(algorithm: Algorithm, key data: NSData) {
         assert(data.length > 0, "key: Data.length must be greater than zero.")
         self.init(algorithm: algorithm, key: data.bytes, keyLength: data.length)
     }
     
     private init(algorithm: Algorithm, key: UnsafePointer<Void>, keyLength: Int) {
         self.algorithm = algorithm
-        HMAC_bridge_Init(context, algorithm.bridgedValue, key, UInt(keyLength))
+        HMAC_bridge_Init(ctx.context, algorithm.bridgedValue, key, UInt(keyLength))
     }
     
     public func update(string: String) -> HMAC {
@@ -61,27 +65,23 @@ public class HMAC {
     }
     
     public func update(data: UnsafePointer<Void>, dataLength: Int) -> HMAC {
-        HMAC_bridge_Update(context, data, UInt(dataLength))
+        HMAC_bridge_Update(ctx.context, data, UInt(dataLength))
         return self
     }
     
     public func final() -> [UInt8] {
         var hmac = Array<UInt8>(count: algorithm.digestLength, repeatedValue:0)
-        HMAC_bridge_Final(context, &hmac)
+        HMAC_bridge_Final(ctx.context, &hmac)
         return hmac
     }
     
     public func finalHexString() -> String {
         return self.dynamicType.hexStringFromArray(final())
     }
-    
-    deinit {
-        context.dealloc(1)
-    }
 }
 
 extension HMAC {
-    private class func convertHexDigit(c: UnicodeScalar) -> UInt8 {
+    private static func convertHexDigit(c: UnicodeScalar) -> UInt8 {
         switch c {
         case UnicodeScalar("0")...UnicodeScalar("9"): return UInt8(c.value - UnicodeScalar("0").value)
         case UnicodeScalar("a")...UnicodeScalar("f"): return UInt8(c.value - UnicodeScalar("a").value + 0xa)
@@ -90,7 +90,7 @@ extension HMAC {
         }
     }
     
-    public class func arrayFromHexString(var s: String) -> [UInt8]{
+    public static func arrayFromHexString(var s: String) -> [UInt8]{
         if s.hasPrefix("0x") {
             s = suffix(s, countElements(s) - 2)
         }
@@ -107,7 +107,7 @@ extension HMAC {
         return a
     }
     
-    public class func hexStringFromArray(a: [UInt8], uppercase: Bool = false) -> String {
+    public static func hexStringFromArray(a: [UInt8], uppercase: Bool = false) -> String {
         return a.map{ String(format: uppercase ? "%02X" : "%02x", $0) }.reduce("", +)
     }
 }
